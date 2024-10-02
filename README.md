23f1eaa31b3fc6abb94f1d6623a2c49a31d1486b
Rahmi <rahmimuaz10@gmail.com>
2024-10-02 13:34:41 +0530

    delete

--- README.md
+++
@@ -1,2 +0,0 @@
-# kandurata_hardware
-An online hardware store system replicates the offerings and services of a traditional hardware store in a digital environment.
--- backend/config/db.js
+++
@@ -1,5 +0,0 @@
-import mongoose from "mongoose";
-
-export const connectDB= async () =>{
-    await mongoose.connect('mongodb+srv://shilma:powerPro123@cluster0.4mpkn.mongodb.net/kandurata_hardware').then(() => console.log("DB Connected"));
-}
